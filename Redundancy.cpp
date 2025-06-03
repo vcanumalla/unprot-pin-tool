dccb05ff05dd59cbdae668c58d135901245a3c7a
Vishal Canumalla <vjc@stanford.edu>
2025-06-02 23:32:38 -0700

    dont like this

--- Redundancy.cpp
+++ Redundancy.cpp
@@ -24,11 +24,12 @@ using std::string;
 UINT64 insCount    = 0; //number of dynamically executed instructions
 UINT64 bblCount    = 0; //number of dynamically executed basic blocks
 UINT64 threadCount = 0; //total number of threads, including main thread
-UINT64 staticUnprotCount = 0; // number of unprotected instructions
-UINT64 dynamicUnprotCount = 0;
-
+UINT64 totalReprotects;
+UINT64 totalUnprotects;
 // reg mapping to whether it is unprotected during instrumentation (True if unprotected)
-std::map<std::string, bool> regUnprotMap;
+std::map<std::string, bool*> regUnprotMap;
+std::map<ADDRINT, UINT64> insReprotectCount;
+std::map<ADDRINT, std::string> insReprotectDisasm;
 std::ostream* out = &cerr;
 /* ===================================================================== */
 // Command line switches
@@ -80,6 +81,47 @@ VOID incCount(UINT64* val)
 // Instrumentation callbacks
 /* ===================================================================== */
 
+// Helper to count reprotections. Add to map if not present, and increment reprotects if it is being changed from unprotected to protected.
+// Increment total unprotects whenever a register is changed to unprotected (even if redundant).
+VOID changeRegStatus(std::string regName, bool unprotect, INS ins) {
+    // changing to unprotected
+    if (unprotect) {
+        
+        totalUnprotects++;
+        // reg in map and unprotected already, do nothing.
+        if (regUnprotMap.find(regName) != regUnprotMap.end() && regUnprotMap[regName]) {
+        }
+        // reg not in map (protected), or protected in map, so we change its value in map to true
+        else {
+            // if it is not in the map, it means it is protected
+
+            regUnprotMap[regName] = 
+        }
+    }
+    else {
+        // changing to protected
+        // reg in map and unprotected, so we change its value in map to false
+        if (regUnprotMap.find(regName) != regUnprotMap.end() && regUnprotMap[regName]) {
+            regUnprotMap[regName] = false;
+
+            totalReprotects++;
+            ADDRINT insAddr = INS_Address(ins);
+            if (insReprotectCount.find(insAddr) == insReprotectCount.end()) {
+                insReprotectCount[insAddr] = 1;
+                insReprotectDisasm[insAddr] = INS_Disassemble(ins); // store disassembly for this instruction
+            }
+            else {
+                insReprotectCount[insAddr]++; // increment reprotection count for this register
+            }
+        }
+        // reg not in map (protected), add to map and mark as protected
+        else if (regUnprotMap.find(regName) == regUnprotMap.end()) {
+            regUnprotMap[regName] = false; // mark as protected
+        }
+        // else, reg is in map and protected, do nothing
+    }
+
+}
 /*!
  * Insert call to the CountBbl() analysis routine before every basic block 
  * of the trace.
@@ -112,6 +154,7 @@ VOID Instruction(INS ins, VOID* v) {
     std::string op = INS_Mnemonic(ins);
 
     // ss op y z
+    
     UINT8 is_ss;
     PIN_SafeCopy(&is_ss, (void*)INS_Address(ins), sizeof(UINT8));
     if (is_ss == 0x36) {
@@ -124,34 +167,76 @@ VOID Instruction(INS ins, VOID* v) {
             if (!INS_OperandIsReg(ins, 1)) {
                 cerr << "Second operand is not a register: " << INS_Disassemble(ins) << endl;
             }
-            regUnprotMap[regName] = true; // mark as unprotected
+            changeRegStatus(regName, true, ins); // mark as unprotected
+            // regUnprotMap[regName] = true; // mark as unprotected
         }
 
         // ss op rd [ptr] --> g[rd] <== g[rd]
         // no work since result stays the same as previous value
-    }
-    else {
-        if (op == "ADD" || op == "SUB" || op == "MUL" || op == "DIV" ||
-            op == "AND" || op == "OR" || op == "XOR") {
-            // op rd rs --> g[rd] <== g[rd] + g[rs]
-            if (INS_OperandCount(ins) == 2 && INS_OperandIsReg(ins, 0) && INS_OperandIsReg(ins, 1)) {
-                // collect the operand names
-                bool val1 = regUnprotMap.find(REG_StringShort(INS_OperandReg(ins, 0))) != regUnprotMap.end() &&
-                            regUnprotMap[REG_StringShort(INS_OperandReg(ins, 0))];
-                bool val2 = regUnprotMap.find(REG_StringShort(INS_OperandReg(ins, 1))) != regUnprotMap.end() &&
-                            regUnprotMap[REG_StringShort(INS_OperandReg(ins, 1))];
-                regUnprotMap[REG_StringShort(INS_OperandReg(ins, 0))] = val1 && val2; // mark as protected if either operand is protected.
-            }
-        }
-        // mov rd [ptr] -> g[rd] <== 0
-        else if (op == "MOV" && INS_OperandCount(ins) == 2 && INS_OperandIsReg(ins, 0) && !INS_OperandIsReg(ins, 1)) {
+        else if (INS_OperandCount(ins) == 2 && INS_OperandIsReg(ins, 0) && !INS_OperandIsReg(ins, 1)) {
             REG reg = INS_OperandReg(ins, 0);
             std::string regName = REG_StringShort(reg);
-            regUnprotMap[regName] = false; // mark as protected
+            if (regUnprotMap.find(regName) == regUnprotMap.end()) {
+                // if the register is not in the map, it means it is protected.
+                // so we add it to the map and mark it as protected.
+                changeRegStatus(regName, false, ins);
+            }
+            // in map and has value, do nothing.
+        }
+        else {
+            cerr << "Unhandled ss instruction: " << INS_Disassemble(ins) << "at " << std::hex << INS_Address(ins) << std::dec << endl;
+        }
+    }
+    else {
+        // if there is an output reg being written to, we check to see what its new value is
+        // op rd rs1 ... rsN
+        if (INS_OperandCount(ins) > 0 && INS_OperandIsReg(ins, 0) && INS_OperandWritten(ins, 0)) {
+            // cerr << "Instruction writing to register: " << INS_Disassemble(ins) << " at " << std::hex << INS_Address(ins) << std::dec << endl;
+
+            bool val = true;
+            if (INS_OperandCount(ins) == 1) {
+                val = false;
+                // err if its not pop or not
+                // if (op != "POP" && op != "NOT") {
+                //     cerr << "Instruction writing to register with no other operands: " << INS_Disassemble(ins) << " at " << std::hex << INS_Address(ins) << std::dec << endl;
+                // }
+            }
+            else {
+                for (UINT32 i = 0; i < INS_OperandCount(ins); i++) {
+                    if (INS_OperandIsReg(ins, i)) {
+                        REG reg = INS_OperandReg(ins, i);
+                        std::string regName = REG_StringShort(reg);
+                        if (regUnprotMap.find(regName) != regUnprotMap.end()) {
+                            val &= regUnprotMap[regName];
+                        }
+                        else {
+                            // its protected if it is not in the map
+                            val = false;
+                            break;
+                        }
+                    }
+                    else if (INS_OperandIsMemory(ins, i)) {
+                        // if the operand is memory, we assume it is protected
+                        val = false;
+                        break;
+                    }
+                }
+            }
+            REG reg = INS_OperandReg(ins, 0);
+            std::string regName = REG_StringShort(reg);
+            
+            // regUnprotMap[regName] = val;
+            changeRegStatus(regName, val, ins); // mark as unprotected if val is true, protected if false
+        }
+        // sub/add/mul... rd [ptr]: g[rd] <== 0
+        else if (INS_OperandCount(ins) == 2 && INS_OperandIsReg(ins, 0) && !INS_OperandIsReg(ins, 1)) {
+            REG reg = INS_OperandReg(ins, 0);
+            std::string regName = REG_StringShort(reg);
+            changeRegStatus(regName, false, ins);
         }
     }
 
-    // sub/add/mul... rd [ptr]
+    
 
 }
 VOID ThreadStart(THREADID threadIndex, CONTEXT* ctxt, INT32 flags, VOID* v) { threadCount++; }
@@ -168,6 +253,28 @@ VOID Fini(INT32 code, VOID* v)
     *out << "===============================================" << endl;
     *out << "MyPinTool analysis results: " << endl;
     *out << "===============================================" << endl;
+    
+    *out << "Total number of executed instructions: " << insCount << endl;
+    *out << "Total number of executed basic blocks: " << bblCount << endl;
+    *out << "Total number of threads: " << threadCount << endl;
+
+    *out << "Total number of register unprotections: " << totalUnprotects << endl;
+    *out << "Total number of register reprotections: " << totalReprotects << endl;
+    *out << "Total number of unique registers unprotected: " << regUnprotMap.size() << endl;
+    *out << "===============================================" << endl;
+    *out << "===============================================" << endl;
+
+    *out << "Reprotections per instruction for csv:" << endl;
+    *out << "Instruction Address,Reprotect Count, Instruction" << endl;
+    for (const auto& entry : insReprotectCount) {
+        ADDRINT addr = entry.first;
+        UINT64 count = entry.second;
+        std::string disasm = insReprotectDisasm[addr];
+        *out << std::hex << addr << "," << count << "," << std::dec << disasm << std::dec << endl;
+    }
+    
+
+
 }
 
 /*!
